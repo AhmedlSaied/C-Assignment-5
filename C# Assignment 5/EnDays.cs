d1e82bef45e84ef26cbfc730f9f66694dd6d9084
Ahmed Mohamed saeed <ahmed_m16709@cic-cairo.com>
2026-05-20 11:37:50 +0300

    Add project files.

---
+++ C# Assignment 5/EnDays.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__Assignment_5
+{
+   public enum EnDays
+    {
+    
+   SATURDAY=0,
+   SUNDAY=1,
+   MONDAY=2,
+   TUESDAY=3,
+   WEDNESDAY=4,
+   THURSDAY=5,
+   FURIDAY=6,
+
+
+    }
+}
+
---
+++ C# Assignment 5/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace C__Assignment_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (!int.TryParse(Console.ReadLine(),out int dayNumber))
+            {
+                return EnDays days = dayNumber;
+                {
+                    EnDays.SATURDAY => "Weekend",
+                    EnDays.MONDAY => "Workday",
+                    // ...
+                };
+            }
+        }
+    }
+}
